1146aa6bcf4ae1b8dfdeb4e2c83c51e872d0947b
Jinto Jose <jintoppy@gmail.com>
2022-04-29 14:12:24 +0200

    initial

---
+++ src/app/util.spec.ts
@@ -0,0 +1,5 @@
+describe('trial', () => {
+    it('should be true', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ src/app/util.ts
@@ -0,0 +1,8 @@
+export const Util = {
+    add: (x: number,y:number) => {
+        return x+y;
+    },
+    sub: (x: number, y:number) => {
+        
+    }
+};
